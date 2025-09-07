41826ce5522d80a2e2ea284ed657b0cc7bfb4ca6
tatiana <tatefrolova@gmail.com>
2025-09-07 13:39:32 +0300

    need to fix

---
+++ src/main/java/org/example/oauth2demoapp/OAuth2DemoAppApplication.java
@@ -0,0 +1,63 @@
+package org.example.oauth2demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+@EnableWebSecurity
+public class OAuth2DemoAppApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2DemoAppApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "index"; //почему-то возвращает просто надпись index
+	}
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests(a -> a
+						.requestMatchers("/", "/error", "/webjars/**").permitAll()
+						.anyRequest().authenticated()
+				)
+				.exceptionHandling(e -> e
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				)
+				.logout(l -> l
+						.logoutSuccessUrl("/").permitAll()
+				)
+				.csrf(c -> c
+						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				)
+				.oauth2Login(oauth2 -> oauth2
+						.defaultSuccessUrl("/", true)
+				);
+
+		return http.build();
+	}
+
+}
---
+++ src/test/java/org/example/oauth2demoapp/OAuth2DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oauth2demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
